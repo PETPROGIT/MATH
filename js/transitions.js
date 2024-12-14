a5924cd337406b02b8af577a3b23200ff964f4c0
PETPROGIT <educatttaccmva54@gmail.com>
2024-12-15 01:49:49 +0700

    Add files via upload

---
+++ js/transitions.js
@@ -0,0 +1,23 @@
+
+
+
+const pages = document.querySelectorAll(".navElement");
+
+pages.forEach(element => {
+    element.addEventListener("click", (event)=>{
+        document.getElementById("homeBtn").classList.remove("active");
+        document.getElementById("taskBtn").classList.remove("active");
+        document.getElementById("adventBtn").classList.remove("active");
+        document.getElementById("shopBtn").classList.remove("active");
+        document.getElementById("infoBtn").classList.remove("active");
+
+
+
+        document.getElementById("homePage").style.display = "none";
+        document.getElementById("infoPage").style.display = "none";
+        
+        document.getElementById(((event.target.id).toString()).slice(0, -1)).classList.add("active");
+        document.getElementById(((event.target.id).toString()).slice(0, -4)+"Page").style.display = "block";
+        
+    })
+});
