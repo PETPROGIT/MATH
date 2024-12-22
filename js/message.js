1345f5a18761a729f28d4ddfa8038e7903220050
PETPROGIT <educatttaccmva54@gmail.com>
2024-12-23 02:55:14 +0700

    Add files via upload

---
+++ js/message.js
@@ -0,0 +1,21 @@
+
+const message = document.getElementById("messageModal");
+
+let condition = 0;
+function showMessage(){
+    if(condition == 0){
+        condition = 1;
+        message.style.visibility = "visible";
+        message.style.paddingTop = "14px";
+        message.style.opacity = "1";
+        setTimeout(() => {
+            message.style.opacity = "0";
+            message.style.visibility = "hidden";
+            message.style.paddingTop = "0px";
+            condition = 0;
+        }, 1200);
+    }
+    else{
+        return false;
+    }
+}
