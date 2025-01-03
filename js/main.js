ab56d7eb5c45607e3c5d5ac810518920473119e8
PETPROGIT <educatttaccmva54@gmail.com>
2025-01-04 05:16:29 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+let balance = 990;
+
+const balanceHTML = document.getElementById("balanceHTML");
+
+balanceHTML.style.fontSize = "54px";
+balanceHTML.style.fontWeight = "900";
+balanceHTML.style.fontFamily = "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-seri";
+
+function updateBalance(){
+
+    balanceSTR = balance.toString();
+    if(balance >= 1000){
+        balanceSTR = (balance.toString())[0]+","+(balance.toString()).slice(1);
+    }
+    if(balance >= 10000){
+        balanceSTR = (balance.toString()).slice(0, 2)+","+(balance.toString()).slice(2);
+    }
+    if(balance >= 100000){
+        balanceSTR = (balance.toString()).slice(0, 3)+","+(balance.toString()).slice(3);
+    }
+    balanceHTML.textContent = balanceSTR;
+
+}
+updateBalance();
---
+++ js/play.js
@@ -0,0 +1,110 @@
+
+
+
+
+
+
+let energy = 0;
+
+const energyProgressBar = document.getElementById("progress");
+const energyBar = document.getElementById("EnergyBar");
+
+
+function energyCheck(){
+    if(energy == 3){
+        energyProgressBar.style.width = "100%";
+    }
+    else if(energy == 2){
+        energyProgressBar.style.width = "66%";
+    }
+    else if(energy == 1){
+        energyProgressBar.style.width = "33%";
+    }
+    else{
+        energyProgressBar.style.width = "0%";
+    }
+}
+
+energyCheck();
+
+
+var music = new Audio('Dancing Cow - Dancing Polish Cow.mp3');
+
+
+const playButtom = document.getElementById("startMining");
+const volumeBtn = document.getElementById("volume");
+
+function stopDancing(){
+    playButtom.classList.remove("mining");
+    volumeBtn.classList.remove("mining");
+    playButtom.innerHTML = '<span class="material-symbols-outlined arrF" style="margin-right: 20px;">arrow_forward</span>Start dancing<span class="material-symbols-outlined arrB" style="margin-left: 20px;">arrow_back</span>';
+    music.pause();
+    document.getElementById("p").src = "d7b48b764442426281d66d511819181fe2pTkDIOtCCuRNGj-1.png";
+    music = new Audio('Dancing Cow - Dancing Polish Cow.mp3');
+    clearInterval(mining);
+    playStop = false;
+    balanceHTML.style.animation = "none";
+    energyProgressBar.style.transition = 'none';
+    energyCheck();
+}
+
+
+
+let mining;
+let playStop = false;
+function play(){
+    if(playStop == false && energy != 0){
+
+        music.play();
+        volumeBtn.innerHTML = '<span class="material-symbols-outlined">volume_up</span>';
+
+        energyProgressBar.style.transition = 'all 243s ease-in-out';
+
+
+        setTimeout(() => {
+            playButtom.classList.add("mining");
+            volumeBtn.classList.add("mining");
+            playButtom.innerHTML = '<span class="material-symbols-outlined arrF" style="margin-right: 15px;">arrow_forward</span>Stop dancing<span class="material-symbols-outlined arrB" style="margin-left: 15px;">arrow_back</span>';
+            document.getElementById("p").src = "img/polish-dancing-cow-dancing.gif";
+            balanceHTML.style.animation = "miningAnimation 1s linear infinite";
+
+            energy--;
+            energyCheck();
+
+        }, 400);
+        setTimeout(() => {
+            music.pause();
+            document.getElementById("p").src = "d7b48b764442426281d66d511819181fe2pTkDIOtCCuRNGj-1.png";
+            music = new Audio('Dancing Cow - Dancing Polish Cow.mp3');
+            clearInterval(mining);
+            stopDancing();
+        }, 243000);
+        mining = setInterval(() => {
+            balance+=1;
+            updateBalance();
+        }, 950);
+        playStop = true;
+    }
+    else if(energy == 0){
+        return false;
+    }
+    else{
+        stopDancing();
+    }
+
+}
+
+
+let vol = true;
+function volumeMusic(){
+    if(vol == true){
+        music.volume = '0';
+        vol = false;
+        volumeBtn.innerHTML = '<span class="material-symbols-outlined">volume_off</span>';
+    }
+    else{
+        music.volume = '1';
+        vol = true;
+        volumeBtn.innerHTML = '<span class="material-symbols-outlined">volume_up</span>';
+    }
+}
