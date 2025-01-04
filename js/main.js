a8fa1de92dd079e4003842d9f374b935bd765e63
PETPROGIT <educatttaccmva54@gmail.com>
2025-01-04 16:36:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,18 +25,3 @@ function updateBalance(){
 }
 updateBalance();
 
-let telegram = window.Telegram.WebApp;
-telegram.expand();
-telegram.disableVerticalSwipes();
-const username = telegram.initDataUnsaf.user.username;
-if(username != undefined){
-    document.getElementById("userName").textContent = username;
-}
-else{
-    document.getElementById("userName").textContent = "NEWUSER";
-}
-
-document.getElementById("userPhoto").src = telegram.initDataUnsafe.user.photo_url;	
-
-
-document.querySelector(".tg").textContent = telegram.initDataUnsafe;
