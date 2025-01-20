70dee5df2a575e96fd1c19c6731887b292c21185
PETPROGIT <educatttaccmva54@gmail.com>
2025-01-20 17:39:26 +0700

    Update js.js

--- js.js
+++ js.js
@@ -69,7 +69,7 @@ if(energy < 100){
         }
         energy=parseInt(energy)+2;
         updateEnergy();
-    }, 60000);
+    }, 10000);
 }
 let energyHTML = document.getElementById("energyBar");
 function updateEnergy(){
@@ -103,7 +103,7 @@ function tap(){
                 energy+=2;
                 updateEnergy();
                 console.log(energy);
-            }, 60000);
+            }, 10000);
         }
     }
 }
